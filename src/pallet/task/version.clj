c0c6ab16eb44269ecff86401fc394d9d8fa7dc4f
Hugo Duncan <hugo_duncan@yahoo.com>
2010-11-16 00:35:06 -0500

    Strip pallet core

--- src/pallet/task/version.clj
+++
@@ -1,9 +0,0 @@
-(ns pallet.task.version
-  "Print Pallet's version to standard out.")
-
-(defn version
-  {:no-service-required true}
-  []
-  (println "Pallet" (System/getProperty "pallet.version")
-           "on Java" (System/getProperty "java.version")
-           (System/getProperty "java.vm.name")))
